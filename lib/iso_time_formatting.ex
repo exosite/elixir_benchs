4637021ef58e4eb6d92bc4065170c981cadbf4de
Orga Shih <orgashih@exosite.com>
2018-03-30 11:05:53 +0800

    init commit

---
+++ lib/iso_time_formatting.ex
@@ -0,0 +1,18 @@
+defmodule IsoTimeFormatting do
+  @moduledoc """
+  Documentation for IsoTimeFormatting.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IsoTimeFormatting.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/iso_time_formatting_test.exs
@@ -0,0 +1,8 @@
+defmodule IsoTimeFormattingTest do
+  use ExUnit.Case
+  doctest IsoTimeFormatting
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
