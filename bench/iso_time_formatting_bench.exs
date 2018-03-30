017cc06140a3e9fa28adbc72fadc12dbaa34a3d6
Cola Cheng <colacheng@exosite.com>
2018-03-30 18:39:37 +0800

    Update the naming.

--- bench/iso_time_formatting_bench.exs
+++ bench/iso_time_formatting_bench.exs
@@ -26,21 +26,19 @@ end
 
 elixir_string_compose = fn ->
   Enum.each(input_datas, fn time ->
-    <<a :: binary-size(4), b :: binary-size(6), c :: binary-size(6)>> = time |> Integer.to_string
-    us = String.to_integer(c)
+    <<mega_secs :: binary-size(4), secs :: binary-size(6), micro_secs :: binary-size(6)>> = time |> Integer.to_string
     {{year, month, day}, {hr, min, sec}} = :calendar.now_to_datetime(
-      {String.to_integer(a), String.to_integer(b), us}
+      {String.to_integer(mega_secs), String.to_integer(secs), String.to_integer(micro_secs)}
     )
-    "#{year}-#{month |> Integer.to_string |> String.rjust(2, ?0)}-#{day |> Integer.to_string |> String.rjust(2, ?0)}T#{hr |> Integer.to_string |> String.rjust(2, ?0)}:#{min |> Integer.to_string |> String.rjust(2, ?0)}:#{sec |> Integer.to_string |> String.rjust(2, ?0)}.#{c}+00:00"
+    "#{year}-#{month |> Integer.to_string |> String.rjust(2, ?0)}-#{day |> Integer.to_string |> String.rjust(2, ?0)}T#{hr |> Integer.to_string |> String.rjust(2, ?0)}:#{min |> Integer.to_string |> String.rjust(2, ?0)}:#{sec |> Integer.to_string |> String.rjust(2, ?0)}.#{micro_secs}+00:00"
   end)
 end
 
 erlang_binary_compose = fn ->
   Enum.each(input_datas, fn time ->
-    <<a :: binary-size(4), b :: binary-size(6), c :: binary-size(6)>> = time |> Integer.to_string
-    us = String.to_integer(c)
+    <<mega_secs :: binary-size(4), secs :: binary-size(6), micro_secs :: binary-size(6)>> = time |> Integer.to_string
     {{year, month, day}, {hr, min, sec}} = :calendar.now_to_datetime(
-      {String.to_integer(a), String.to_integer(b), us}
+      {String.to_integer(mega_secs), String.to_integer(secs), String.to_integer(micro_secs)}
     )
     year_b = year |> :erlang.integer_to_binary
     month_b = :erlang.integer_to_list(month) |> :string.right(2, ?0) |> :erlang.list_to_binary
@@ -48,7 +46,16 @@ erlang_binary_compose = fn ->
     hr_b = :erlang.integer_to_list(hr) |> :string.right(2, ?0) |> :erlang.list_to_binary
     min_b = :erlang.integer_to_list(min) |> :string.right(2, ?0) |> :erlang.list_to_binary
     sec_b = :erlang.integer_to_list(sec) |> :string.right(2, ?0) |> :erlang.list_to_binary
-    <<year_b :: binary-size(4), "-", month_b :: binary-size(2), "-", day_b :: binary-size(2), "T", hr_b :: binary-size(2), ":", min_b :: binary-size(2), ":", sec_b :: binary-size(2), ".", c :: binary-size(6), "+00:00">>
+    <<
+      year_b :: binary-size(4),
+      "-", month_b :: binary-size(2),
+      "-", day_b :: binary-size(2),
+      "T", hr_b :: binary-size(2),
+      ":", min_b :: binary-size(2),
+      ":", sec_b :: binary-size(2),
+      ".", micro_secs :: binary-size(6),
+      "+00:00"
+    >>
   end)
 end
 
@@ -60,15 +67,14 @@ progressiv_erlang_binary_compose = fn ->
         h_ms = rem(time, 86_400_000_000)
         m_ms = rem(h_ms, 3600_000_000)
         s_ms = rem(m_ms, 60_000_000)
-        c = rem(s_ms, 1_000_000) |> :erlang.integer_to_list() |> :string.right(6, ?0) |> :erlang.list_to_binary
+        micro_secs = rem(s_ms, 1_000_000) |> :erlang.integer_to_list() |> :string.right(6, ?0) |> :erlang.list_to_binary
         sec = div(s_ms, 1_000_000)
         min = div(m_ms, 60_000_000)
         hr = div(h_ms, 3600_000_000)
       other ->
-        <<a :: binary-size(4), b :: binary-size(6), c :: binary-size(6)>> = time |> Integer.to_string
-        us = String.to_integer(c)
+        <<mega_secs :: binary-size(4), secs :: binary-size(6), micro_secs :: binary-size(6)>> = time |> Integer.to_string
         {{year, month, day}, {hr, min, sec}} = :calendar.now_to_datetime(
-          {String.to_integer(a), String.to_integer(b), us}
+          {String.to_integer(mega_secs), String.to_integer(secs), String.to_integer(micro_secs)}
         )
         year_b = year |> :erlang.integer_to_binary
         month_b = :erlang.integer_to_list(month) |> :string.right(2, ?0) |> :erlang.list_to_binary
@@ -81,7 +87,14 @@ progressiv_erlang_binary_compose = fn ->
     min_b = :erlang.integer_to_list(min) |> :string.right(2, ?0) |> :erlang.list_to_binary
     sec_b = :erlang.integer_to_list(sec) |> :string.right(2, ?0) |> :erlang.list_to_binary
 
-    <<day_string :: binary-size(11), hr_b :: binary-size(2), ":", min_b :: binary-size(2), ":", sec_b :: binary-size(2), ".", c :: binary-size(6), "+00:00">>
+    <<
+      day_string :: binary-size(11),
+      hr_b :: binary-size(2),
+      ":", min_b :: binary-size(2),
+      ":", sec_b :: binary-size(2),
+      ".", micro_secs :: binary-size(6),
+      "+00:00"
+    >>
     {last, day_string}
   end)
 end
