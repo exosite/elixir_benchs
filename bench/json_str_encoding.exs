4c20ada1ab0d84a5dfd7c784c7f7c57141c3e99d
Renaud Sauvain <renaudsauvain@exosite.com>
2018-10-12 15:03:36 +0800

    Add sub json to test

--- bench/json_str_encoding.exs
+++ bench/json_str_encoding.exs
@@ -31,7 +31,17 @@ string_obj = fn ->
     "\",\"message\":" <> numb <>
     ",\"code\":" <> numb <>
     ",\"data\":{" <>
-    "\"processing_time\":" <> numb <>
+    ",\"request\":" <> :jiffy.encode(%{
+      "a" => 1,
+      "b" => 2,
+      "c" => 3
+    }) <>
+    ",\"response\":" <> :jiffy.encode(%{
+      "d" => 4,
+      "e" => 5,
+      "f" => 6
+    }) <>
+    ",\"processing_time\":" <> numb <>
     ",\"data_in\":" <> numb <>
     ",\"data_out\":" <> numb <>
     "}}"
@@ -41,7 +51,8 @@ end
 list_obj = fn ->
   Enum.map(1..1000, fn numb ->
     numb = Integer.to_string(numb)
-    <<"{\"type\":\"call\",\"severity\":\"",severity::binary,"\",\"timestamp\":",numb::binary,",\"elapsed\":",numb::binary,",\"tracking_id\":\"",severity::binary,"\",\"solution_id\":\"",severity::binary,"\",\"service\":\"",severity::binary,"\",\"service_type\":\"",severity::binary,"\",\"event\":\"",severity::binary,"\",\"message\":",numb::binary,",\"code\":",numb::binary,",\"data\":{\"processing_time\":",numb::binary,",\"data_in\":",numb::binary,",\"data_out\":",numb::binary,"}}">>
+    <<"{\"type\":\"call\",\"severity\":\"",severity::binary,"\",\"timestamp\":",
+    numb::binary,",\"elapsed\":",numb::binary,",\"tracking_id\":\"",severity::binary,"\",\"solution_id\":\"",severity::binary,"\",\"service\":\"",severity::binary,"\",\"service_type\":\"",severity::binary,"\",\"event\":\"",severity::binary,"\",\"message\":",numb::binary,",\"code\":",numb::binary,",\"data\":{\"processing_time\":",numb::binary,",\"data_in\":",numb::binary,",\"data_out\":",numb::binary,"}}">>
   end)
 end
 
@@ -60,6 +71,16 @@ jiffy_obj = fn ->
     "message" => numb,
     "code" => numb,
     "data" => %{
+      "request" => %{
+        "a" => 1,
+        "b" => 2,
+        "c" => 3
+      },
+      "response" => %{
+        "d" => 4,
+        "e" => 5,
+        "f" => 6
+      },
       "processing_time" => numb,
       "data_in" => numb,
       "data_out" => numb,
@@ -113,7 +134,7 @@ end
 
 # Start benchmark
 Benchee.run(%{
-  "jiffy" => jiffy_obj,
+  "jiffy_obj" => jiffy_obj,
   "jiffy_obj_atom" => jiffy_obj_atom,
   "inspect" => inspect_obj,
   "list" => list_obj,
