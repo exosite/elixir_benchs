5e3a7ad55fcaf67ea1586667a0445904a1711eb6
Renaud Sauvain <renaudsauvain@exosite.com>
2019-10-30 11:55:20 +0800

    up RFC2822

--- bench/date_rfc2822_format.exs
+++ bench/date_rfc2822_format.exs
@@ -1,60 +1,106 @@
 # Iso format vs custom
 
 days = %{
-  1 => "Mon",
-  2 => "Tue",
-  3 => "Wed",
-  4 => "Thu",
-  5 => "Fri",
-  6 => "Sat",
-  7 => "Sun"
+  1 => "Mon, ",
+  2 => "Tue, ",
+  3 => "Wed, ",
+  4 => "Thu, ",
+  5 => "Fri, ",
+  6 => "Sat, ",
+  7 => "Sun, "
 }
 
 months = %{
-  1 => "Jan",
-  2 => "Feb",
-  3 => "Mar",
-  4 => "Apr",
-  5 => "May",
-  6 => "Jun",
-  7 => "Jul",
-  8 => "Aug",
-  9 => "Sep",
-  10 => "Oct",
-  11 => "Nov",
-  12 => "Dec"
+  1 => " Jan ",
+  2 => " Feb ",
+  3 => " Mar ",
+  4 => " Apr ",
+  5 => " May ",
+  6 => " Jun ",
+  7 => " Jul ",
+  8 => " Aug ",
+  9 => " Sep ",
+  10 => " Oct ",
+  11 => " Nov ",
+  12 => " Dec "
 }
 
-day_key_offset = 89435
-month_key_offset = 74558
-
-Enum.each(days, fn {i, v} ->
-  :persistent_term.put(day_key_offset + i, v)
-end)
-Enum.each(months, fn {i, v} ->
-  :persistent_term.put(month_key_offset + i, v)
-end)
+# Using persistent_term have same perf as function case
+# Enum.each(days, fn {i, v} ->
+#   :persistent_term.put({:day, i}, v)
+# end)
+# Enum.each(months, fn {i, v} ->
+#   :persistent_term.put({:month, i}, v)
+# end)
 
 io_lib = fn ->
   Enum.each(1..1000, fn _ ->
     %{day: d, month: m, year: y, hour: h, minute: min, second: s} = now = DateTime.utc_now()
-    day = :persistent_term.get(day_key_offset + Date.day_of_week(now))
-    day <> (:io_lib.format("~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w.000Z", [y, m, d, h, min, s]) |> to_string)
+    days[Date.day_of_week(now)] <> (:io_lib.format("~.4.0w-~.2.0w-~.2.0w ~.2.0w:~.2.0w:~.2.0w GMT", [y, m, d, h, min, s]) |> to_string)
   end)
 end
 
-custom = fn ->
+custom_map = fn ->
   Enum.each(1..1000, fn _ ->
     %{day: d, month: m, year: y, hour: h, minute: min, second: s} = now = DateTime.utc_now()
-    dw = Date.day_of_week(now)
     <<
-    :persistent_term.get(day_key_offset + dw)::binary-size(3),
-    ", ",
+    days[Date.day_of_week(now)]::binary-size(5),
     div(h, d) + 48,
     rem(h, d) + 48,
+    months[m]::binary-size(5),
+    div(y, 1000) + 48,
+    rem(div(y, 100), 10) + 48,
+    rem(div(y, 10), 10) + 48,
+    rem(y, 10) + 48,
     " ",
-    :persistent_term.get(month_key_offset + m)::binary-size(3),
-    " ",
+    div(h, 10) + 48,
+    rem(h, 10) + 48,
+    ":",
+    div(min, 10) + 48,
+    rem(min, 10) + 48,
+    ":",
+    div(s, 10) + 48,
+    rem(s, 10) + 48,
+    " GMT"
+    >>
+  end)
+end
+
+custom_pattern = fn ->
+  Enum.each(1..1000, fn _ ->
+    %{day: d, month: m, year: y, hour: h, minute: min, second: s} = now = DateTime.utc_now()
+
+    # Note: using a function get_day.(i) with matching param is a little slower
+    dws = case Date.day_of_week(now) do
+      1 -> "Mon, "
+      2 -> "Tue, "
+      3 -> "Wed, "
+      4 -> "Thu, "
+      5 -> "Fri, "
+      6 -> "Sat, "
+      7 -> "Sun, "
+    end
+
+    ms = case m do
+      1 -> " Jan "
+      2 -> " Feb "
+      3 -> " Mar "
+      4 -> " Apr "
+      5 -> " May "
+      6 -> " Jun "
+      7 -> " Jul "
+      8 -> " Aug "
+      9 -> " Sep "
+      10 -> " Oct "
+      11 -> " Nov "
+      12 -> " Dec "
+    end
+
+    <<
+    dws::binary-size(5),
+    div(h, d) + 48,
+    rem(h, d) + 48,
+    ms::binary-size(5),
     div(y, 1000) + 48,
     rem(div(y, 100), 10) + 48,
     rem(div(y, 10), 10) + 48,
@@ -74,6 +120,7 @@ custom = fn ->
 end
 
 Benchee.run(%{
-  "custom" => custom,
   "io_lib" => io_lib,
+  "custom_map" => custom_map,
+  "custom_pattern" => custom_pattern,
 }, time: 10, parallel: 1)
