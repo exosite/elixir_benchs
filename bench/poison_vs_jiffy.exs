83140acab7a1ae63623b0d539562fdf802389ba0
Renaud Sauvain <renaudsauvain@exosite.com>
2020-05-05 14:40:22 +0800

    more tests

--- bench/poison_vs_jiffy.exs
+++ bench/poison_vs_jiffy.exs
@@ -1,5 +1,5 @@
 obj = %{ "asdfsadf" => ["str", 123, %{ "sub" => "asfdsaf" }, :atom] }
-str = :jiffy.encode(obj)
+str = :jiffy.encode({obj})
 
 jiffy_enc = fn ->
   Enum.each(1..1000, fn _ ->
@@ -10,6 +10,12 @@ end
 poison_enc = fn ->
   Enum.each(1..1000, fn _ ->
     Poison.encode!(obj)
+  end)
+end
+
+poison_io_enc = fn ->
+  Enum.each(1..1000, fn _ ->
+    Poison.encode_to_iodata!(obj)
   end)
 end
 
@@ -26,8 +32,9 @@ poison_dec = fn ->
 end
 
 Benchee.run(%{
-  "jiffy decoding" => jiffy_dec,
-  "poison decoding" => poison_dec,
+  # "jiffy decoding" => jiffy_dec,
+  # "poison decoding" => poison_dec,
   "jiffy encoding" => jiffy_enc,
-  "poison encoding" => poison_enc
+  "poison encoding" => poison_enc,
+  "poison io encoding" => poison_io_enc
 }, time: 10, parallel: 1)
